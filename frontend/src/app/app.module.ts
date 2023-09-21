3714919213365a548af3bfea85c82fca44376b8c
grey-monkii <chanzyongco@gmail.com>
2023-09-21 14:04:49 +0800

    signup partial

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { ToastrModule } from 'ngx-toastr';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,6 +12,34 @@ import { StarRatingComponent } from './components/partials/star-rating/star-rati
 import { SearchComponent } from './components/partials/search/search.component';
 import { TagsComponent } from './components/partials/tags/tags.component';
 import { LoginComponent } from './components/pages/login/login.component';
+import { SignupComponent } from './components/pages/signup/signup.component';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCx3nRd7cSvuYV-8xaDx1LbJKnGLT27e54",
+  authDomain: "caterpillar-hestia.firebaseapp.com",
+  projectId: "caterpillar-hestia",
+  storageBucket: "caterpillar-hestia.appspot.com",
+  messagingSenderId: "457506187045",
+  appId: "1:457506187045:web:5f389ea93701f5808d5ccc",
+  measurementId: "G-QCVQ7KTVTF",
+  databaseURL: "https://caterpillar-hestia-default-rtdb.asia-southeast1.firebasedatabase.app/"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
+const database = getDatabase(app);
+
 
 @NgModule({
   declarations: [
@@ -18,11 +49,14 @@ import { LoginComponent } from './components/pages/login/login.component';
     StarRatingComponent,
     SearchComponent,
     TagsComponent,
-    LoginComponent
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ToastrModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
