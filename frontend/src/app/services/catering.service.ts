5ea1d44603b89cc5bac88217b66f724523fe7137
unknown <norbz.vergara.27@gmail.com>
2023-09-22 20:57:28 +0800

    Backend Integration with url

--- frontend/src/app/services/catering.service.ts
+++ frontend/src/app/services/catering.service.ts
@@ -2,13 +2,15 @@ import { Injectable } from '@angular/core';
 import { sample_catering, sample_tags } from 'src/data';
 import { Catering } from '../shared/models/Catering';
 import { Tag } from '../shared/models/Tag';
+import { HttpClient } from '@angular/common/http';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class CateringService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
   getAll():Catering[]{
     return sample_catering;
@@ -27,5 +29,9 @@ export class CateringService {
     this.getAll():
     this.getAll().filter(Catering => Catering.tags?.includes(tag));
 
+  }
+
+  getAllCateringById(cateringID:string): Catering {
+    return this.getAll().find(Catering => Catering.id == cateringID) ?? new Catering();
   }
 }
