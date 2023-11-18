4ebac6a7f4157f850ef9fb0cf7b42a71666ec9ed
baniladjimkenn2301 <137855877+baniladjimkenn2301@users.noreply.github.com>
2023-11-18 11:50:57 +0800

    User profile related

--- frontend/src/app/components/pages/user-info/user-info.component.ts
+++ frontend/src/app/components/pages/user-info/user-info.component.ts
@@ -1,10 +1,54 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 @Component({
-  selector: 'app-user-info',
+  selector: 'app-user-profile',
   templateUrl: './user-info.component.html',
   styleUrls: ['./user-info.component.css']
 })
 export class UserInfoComponent {
+  months: string[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  days: number[] = Array.from({ length: 31 }, (_, i) => i + 1); // Default to 31 days
+  years: number[] = Array.from({ length: 122 }, (_, i) => new Date().getFullYear() - i); // For example, from the current year to 1900
 
+  selectedImage: File=null;
+  selectedImageSrc:  string = null;
+
+  constructor(private router: Router) { }
+
+  goBack() {
+    this.router.navigate(['/dashboard-user']);
+  }
+  
+  onImageSelected(event: any) {
+    const file = event.target.files[0];
+    if (file) {
+      this.selectedImage = file;
+    }
+
+    const reader = new FileReader();
+    reader.onload = (e: any) => {
+      this.selectedImageSrc = e.target.result;
+    };
+    reader.readAsDataURL(file);
+  }
+
+  populateDateDropdown() {
+    const monthDropdown = document.getElementById('month') as HTMLSelectElement;
+    const selectedMonth = monthDropdown.value;
+
+    if (selectedMonth === 'February') {
+      this.days = Array.from({ length: 28 }, (_, i) => i + 1); // Typically February has 28 days
+      // Add logic for leap year if needed
+    } else if (['April', 'June', 'September', 'November'].includes(selectedMonth)) {
+      this.days = Array.from({ length: 30 }, (_, i) => i + 1); // These months have 30 days
+    } else {
+      this.days = Array.from({ length: 31 }, (_, i) => i + 1); // Default to 31 days
+    }
+  }
+  // logout() {
+  //   this.AuthSer.SignOut();
+  //   // Redirect or handle post-logout logic here
+  // }
 }
+
