98e04aa0d06986bf133d1663e786277370861f92
baniladjimkenn2301 <137855877+baniladjimkenn2301@users.noreply.github.com>
2023-11-22 04:12:45 +0800

    next changes

--- frontend/src/app/components/pages/user-info/user-info.component.ts
+++ frontend/src/app/components/pages/user-info/user-info.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { AuthService } from 'src/app/services/auth.service';
+import { FormGroup } from '@angular/forms'; // Import FormGroup
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-info.component.html',
@@ -23,6 +23,62 @@ export class UserInfoComponent {
     username: '',
     password: ''
   };
+  editMode: boolean = false;
+  isFieldDisabled: boolean = true; // Assuming this is used for form field enabling/disabling
+  myForm: FormGroup; // Declare this if using reactive forms
+
+  enableEditMode() {
+    this.editMode = true;
+    this.isFieldDisabled = false;
+
+    if (this.myForm) {
+      this.myForm.enable(); // Enable all controls in the form group
+    }
+
+    // Additional UI changes can be handled via Angular templates
+  }
+  saveChanges() {
+    if (this.myForm) {
+      const formData = this.myForm.value; // Get form data from FormGroup
+      console.log('Saving changes...', formData);
+      // Send formData to the server...
+      this.myForm.reset(); // Reset the form after saving
+    }
+
+    this.editMode = false;
+    this.isFieldDisabled = true;
+
+    // Additional UI updates can be handled via Angular templates
+  }
+  someMethod() {
+  if (this.editMode) {
+    // Perform actions relevant to edit mode
+    // Example: Enable all input fields for editing
+    const inputFields = document.querySelectorAll('.editable-input');
+    inputFields.forEach(field => field.removeAttribute('disabled'));
+
+    // You can also change the UI to reflect that the user is in edit mode
+    // For example, changing the color of a header or showing additional instructions
+    const header = document.getElementById('header');
+    if (header) {
+      header.style.backgroundColor = 'lightblue'; // Example color change
+    }
+  } else {
+    // Perform actions for non-edit mode
+    // Example: Disable all input fields to prevent editing
+    const inputFields = document.querySelectorAll('.editable-input');
+    inputFields.forEach(field => field.setAttribute('disabled', 'true'));
+
+    // Revert any UI changes made during edit mode
+    const header = document.getElementById('header');
+    if (header) {
+      header.style.backgroundColor = ''; // Revert color change
+    }
+
+    // Additional logic for when the user exits edit mode
+    // For example, saving data, validating input, or updating the view
+  }
+}
   onSubmit() {
     // Process login here
     console.log('User Credentials:', this.userCredentials);
@@ -83,6 +139,7 @@ export class UserInfoComponent {
       this.days = Array.from({ length: 31 }, (_, i) => i + 1); // Default to 31 days
     }
   }
+  
   // logout() {
   //   this.AuthSer.SignOut();
   //   // Redirect or handle post-logout logic here
